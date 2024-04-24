785e39b712c36e4272eba920216b20d0f4ec3500
Daniel Bugl <me@omnidan.net>
2024-04-24 16:31:56 +0200

    small fix

--- ch6/backend/src/index.js
+++ ch6/backend/src/index.js
@@ -4,9 +4,8 @@ dotenv.config()
 import { app } from './app.js'
 import { initDatabase } from './db/init.js'
 
-await initDatabase()
-
 try {
+  await initDatabase()
   const PORT = process.env.PORT
   app.listen(PORT)
   console.info(`express server running on http://localhost:${PORT}`)
